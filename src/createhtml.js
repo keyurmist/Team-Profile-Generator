8c1c408a96655e0715f23c5ce8b598dc83d669a1
KeyurMistry <keyurmist@gmail.com>
2022-06-19 22:34:52 +0100

    created manager function

--- src/createhtml.js
+++ src/createhtml.js
@@ -30,3 +30,20 @@ const createTeam = function (employees) {
     </html>
   `;
 };
+
+const createManager = function (manager) {
+  return `
+    <div class="col-4 mt-4">
+        <div class="card h-100">
+            <div class="card-header">
+                <h3>${manager.name}</h3>
+            </div>
+            <div class="card-body">
+                <p class="id">ID: ${manager.id}</p>
+                <p class="email">Email: <a href="mailto:${manager.email}">${manager.email}</a></p>
+                <p class="office">Office Number: ${manager.officeNumber}</p>
+            </div>
+        </div>
+    </div>
+    `;
+};
