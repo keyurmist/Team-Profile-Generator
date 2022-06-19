e81a4d6d417a92b1ceed6bc700006fcd4aeeaf6f
KeyurMistry <keyurmist@gmail.com>
2022-06-19 21:22:01 +0100

    added check to test get methods in employee

--- src/lib/Employee.test.js
+++ src/lib/Employee.test.js
@@ -9,3 +9,25 @@ describe("creates new Employee object", () => {
     expect(employee.email).toEqual(expect.any(String));
   });
 });
+
+//Tests to see if get methods work
+
+test("name", () => {
+  const employee = new Employee("Keyur");
+  expect(employee.getName()).toEqual(expect.any(String));
+});
+
+test("checking id method", () => {
+  const employee = new Employee("Keyur", 420, "keyurmist@gmail.com");
+  expect(employee.getId()).toBe(employee.id);
+});
+
+test("checking getEmail method.", () => {
+  const employee = new Employee("Keyur", 420, "keyurmist@gmail.com");
+  expect(employee.getEmail()).toBe(employee.email);
+});
+
+test("checking role.", () => {
+  const employee = new Employee("Keyur", 420, "keyurmist@gmail.com");
+  expect(employee.getRole()).toBe("Employee");
+});
