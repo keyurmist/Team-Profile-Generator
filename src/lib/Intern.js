b1249270cb4e215aa05f56c1a6702e7c7a05a316
KeyurMistry <keyurmist@gmail.com>
2022-06-19 15:49:45 +0100

    created and exported Intern constructor

---
+++ src/lib/Intern.js
@@ -0,0 +1,19 @@
+const Employee = require("./Employee");
+
+//Again, Intern constructor extends the Employee
+class Intern extends Employee {
+  constructor(name, id, email, school) {
+    super(name, id, email);
+    this.school = school;
+  }
+
+  getSchool() {
+    return this.school;
+  }
+
+  getRole() {
+    return "Intern"; // This overrides Employee to Intern
+  }
+}
+
+module.exports = Intern;
