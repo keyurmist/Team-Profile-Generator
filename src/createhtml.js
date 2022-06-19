6ffdc3b84cf8be2c26871fac3fc9b06427ad3e19
KeyurMistry <keyurmist@gmail.com>
2022-06-19 22:41:03 +0100

    created engineer and intern function

--- src/createhtml.js
+++ src/createhtml.js
@@ -47,3 +47,37 @@ const createManager = function (manager) {
     </div>
     `;
 };
+
+const createEngineer = function (engineer) {
+  return `
+    <div class="col-4 mt-4">
+        <div class="card h-100">
+            <div class="card-header">
+                <h3>${engineer.name}</h3>
+            </div>
+            <div class="card-body">
+                <p class="id">ID: ${engineer.id}</p>
+                <p class="email">Email: <a href="mailto:${engineer.email}">${engineer.email}</a></p>
+                <p class="github">Github: <a href="https://github.com/${engineer.github}">${engineer.github}</a></p>
+            </div>
+        </div>
+    </div>
+    `;
+};
+
+const createIntern = function (intern) {
+  return `
+    <div class="col-4 mt-4">
+        <div class="card h-100">
+            <div class="card-header">
+                <h3>${intern.name}</h3>
+            </div>
+            <div class="card-body">
+                <p class="id">ID: ${intern.id}</p>
+                <p class="email">Email:<a href="mailto:${intern.email}">${intern.email}</a></p>
+                <p class="school">School: ${intern.school}</p>
+            </div>
+    </div>
+</div>
+    `;
+};
