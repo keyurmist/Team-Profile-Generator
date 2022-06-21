2518e8396ec9351bb316153615981ae4577f038f
KeyurMistry <keyurmist@gmail.com>
2022-06-21 12:56:53 +0100

    added statements to push either engineer or intern

--- src/index.js
+++ src/index.js
@@ -65,74 +65,105 @@ const promptManager = () => {
 };
 
 const promptEmployee = () => {
-  return inquirer.prompt([
-    {
-      type: "list",
-      name: "role",
-      message: "Would you liek to add an employee?",
-      choices: ["Engineer, Intern, quit"],
-    },
-
-    {
-      type: "input",
-      name: "name",
-      message: "What is the employee's name?",
-      validate: async (answer) => {
-        if (!answer) {
-          return "Please enter a valid name";
-        }
-        return true;
+  return inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "role",
+        message: "Would you liek to add an employee?",
+        choices: ["Engineer, Intern, quit"],
       },
-    },
 
-    {
-      type: "input",
-      name: "id",
-      message: "Please enter the employee's ID",
-      validate: async (answer) => {
-        if (!answer) {
-          return "Please enter a valid ID";
-        }
-        return true;
+      {
+        type: "input",
+        name: "name",
+        message: "What is the employee's name?",
+        validate: async (answer) => {
+          if (!answer) {
+            return "Please enter a valid name";
+          }
+          return true;
+        },
       },
-    },
 
-    {
-      type: "input",
-      name: "email",
-      message: "Please enter the employee's email",
-      validate: async (answer) => {
-        if (!answer) {
-          return "Please enter a valid email";
-        }
-        return true;
+      {
+        type: "input",
+        name: "id",
+        message: "Please enter the employee's ID",
+        validate: async (answer) => {
+          if (!answer) {
+            return "Please enter a valid ID";
+          }
+          return true;
+        },
       },
-    },
 
-    {
-      type: "input",
-      name: "github",
-      message: "Please enter the employee's github",
-      validate: async (answer) => {
-        if (!answer) {
-          return "Please enter a valid username";
-        }
-        return true;
+      {
+        type: "input",
+        name: "email",
+        message: "Please enter the employee's email",
+        validate: async (answer) => {
+          if (!answer) {
+            return "Please enter a valid email";
+          }
+          return true;
+        },
       },
-      when: (answer) => answer.role === "Engineer",
-    },
 
-    {
-      type: "input",
-      name: "school",
-      message: "Please enter the employee's school",
-      validate: async (answer) => {
-        if (!answer) {
-          return "Please enter a valid school";
-        }
-        return true;
+      {
+        type: "input",
+        name: "github",
+        message: "Please enter the employee's github",
+        validate: async (answer) => {
+          if (!answer) {
+            return "Please enter a valid username";
+          }
+          return true;
+        },
+        when: (answer) => answer.role === "Engineer",
       },
-      when: (answer) => answer.role === "Intern",
-    },
-  ]);
+
+      {
+        type: "input",
+        name: "school",
+        message: "Please enter the employee's school",
+        validate: async (answer) => {
+          if (!answer) {
+            return "Please enter a valid school";
+          }
+          return true;
+        },
+        when: (answer) => answer.role === "Intern",
+      },
+
+      {
+        type: "confirm",
+        name: "anotherEmployee",
+        message: "Would you like to add more members?",
+        default: false,
+      },
+    ])
+    .then((employeeAnswer) => {
+      let { name, id, email, role, github, school, anotherEmployee } =
+        employeeAnswer;
+      let employee;
+
+      if (role === "Engineer") {
+        employee = new Engineer(name, id, email, github);
+
+        console.log(employee);
+      } else if (role === "Intern") {
+        employee = new Intern(name, id, email, school);
+
+        console.log(employee);
+      }
+
+      teamMembers.push(employee);
+
+      if (anotherEmployee) {
+        return promptEmployee(teamMembers);
+      } else {
+        return teamMembers;
+      }
+    });
 };
