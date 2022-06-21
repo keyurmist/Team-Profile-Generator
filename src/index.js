f1f79411768026966b00b27c8e1232776ba033f4
KeyurMistry <keyurmist@gmail.com>
2022-06-21 12:01:09 +0100

    started manager prompts

--- src/index.js
+++ src/index.js
@@ -4,3 +4,21 @@ const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const createHTML = require("./createhtml");
+
+const teamMembers = [];
+
+const promptManager = () => {
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "What is the manager's name?",
+      validate: async (answer) => {
+        if (!answer) {
+          return "Please enter a valid name";
+        }
+        return true;
+      },
+    },
+  ]);
+};
