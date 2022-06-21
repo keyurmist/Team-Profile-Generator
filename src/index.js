bff20ccc4bf68b331440fb3079410f36bd136146
KeyurMistry <keyurmist@gmail.com>
2022-06-21 12:09:12 +0100

    added engineer and intern prompts

--- src/index.js
+++ src/index.js
@@ -58,3 +58,107 @@ const promptManager = () => {
     },
   ]);
 };
+
+const promptEngineer = () => {
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "What is the engineer's name?",
+      validate: async (answer) => {
+        if (!answer) {
+          return "Please enter a valid name";
+        }
+        return true;
+      },
+    },
+
+    {
+      type: "input",
+      name: "id",
+      message: "Please enter the engineer's ID",
+      validate: async (answer) => {
+        if (!answer) {
+          return "Please enter a valid ID";
+        }
+        return true;
+      },
+    },
+
+    {
+      type: "input",
+      name: "email",
+      message: "Please enter the engineer's email",
+      validate: async (answer) => {
+        if (!answer) {
+          return "Please enter a valid email";
+        }
+        return true;
+      },
+    },
+
+    {
+      type: "input",
+      name: "github",
+      message: "Please enter the engineer's github",
+      validate: async (answer) => {
+        if (!answer) {
+          return "Please enter a valid username";
+        }
+        return true;
+      },
+    },
+  ]);
+};
+
+const promptIntern = () => {
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "What is the intern's name?",
+      validate: async (answer) => {
+        if (!answer) {
+          return "Please enter a valid name";
+        }
+        return true;
+      },
+    },
+
+    {
+      type: "input",
+      name: "id",
+      message: "Please enter the intern's ID",
+      validate: async (answer) => {
+        if (!answer) {
+          return "Please enter a valid ID";
+        }
+        return true;
+      },
+    },
+
+    {
+      type: "input",
+      name: "email",
+      message: "Please enter the intern's email",
+      validate: async (answer) => {
+        if (!answer) {
+          return "Please enter a valid email";
+        }
+        return true;
+      },
+    },
+
+    {
+      type: "input",
+      name: "school",
+      message: "Please enter the intern's school",
+      validate: async (answer) => {
+        if (!answer) {
+          return "Please enter a valid school";
+        }
+        return true;
+      },
+    },
+  ]);
+};
