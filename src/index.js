df11b9df03a4d011e57f3d500f488c790c953d9a
KeyurMistry <keyurmist@gmail.com>
2022-06-21 12:42:52 +0100

    altered manager prompt

--- src/index.js
+++ src/index.js
@@ -8,69 +8,69 @@ const createHTML = require("./createhtml");
 const teamMembers = [];
 
 const promptManager = () => {
-  return inquirer.prompt([
-    {
-      type: "input",
-      name: "name",
-      message: "What is the manager's name?",
-      validate: async (answer) => {
-        if (!answer) {
-          return "Please enter a valid name";
-        }
-        return true;
+  return inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "What is the manager's name?",
+        validate: async (answer) => {
+          if (!answer) {
+            return "Please enter a valid name";
+          }
+          return true;
+        },
       },
-    },
+      {
+        type: "input",
+        name: "id",
+        message: "Please enter the manager's ID.",
+        validate: async (answer) => {
+          if (!answer) {
+            return "Please enter a valid ID";
+          }
+          return true;
+        },
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "Please enter the manager's email.",
+        validate: async (answer) => {
+          if (!answer) {
+            return "Please enter a valid email";
+          }
+          return true;
+        },
+      },
+      {
+        type: "input",
+        name: "officeNumber",
+        message: "Please enter the manager's office number",
+        validate: async (answer) => {
+          if (!answer) {
+            return "Please enter a valid office number";
+          }
+          return true;
+        },
+      },
+    ])
+    .then((managerAnswer) => {
+      const { name, id, email, officeNumber } = managerAnswer;
+      const manager = new Manager(name, id, email, officeNumber);
 
-    {
-      type: "input",
-      name: "id",
-      message: "Please enter the manager's ID",
-      validate: async (answer) => {
-        if (!answer) {
-          return "Please enter a valid ID";
-        }
-        return true;
-      },
-    },
-
-    {
-      type: "input",
-      name: "email",
-      message: "Please enter the manager's email",
-      validate: async (answer) => {
-        if (!answer) {
-          return "Please enter a valid email";
-        }
-        return true;
-      },
-    },
-
-    {
-      type: "input",
-      name: "officeNumber",
-      message: "Please enter the manager's office number",
-      validate: async (answer) => {
-        if (!answer) {
-          return "Please enter a valid number";
-        }
-        return true;
-      },
-    },
-  ]);
-  .then((answerManager) => {
-    const manager = new Manager(name, id, email, officeNumber);
-    teamMembers.push(manager);
-    console.log(manager);
-  });
+      teamArray.push(manager);
+      console.log(manager);
+    });
 };
 
 const promptEmployee = () => {
   return inquirer.prompt([
     {
-        type: 'input',
-        name: 'role',
-        message: 'Would you liek to add an employee?',
-        choices: ['Engineer, Intern, quit']
+      type: "list",
+      name: "role",
+      message: "Would you liek to add an employee?",
+      choices: ["Engineer, Intern, quit"],
     },
 
     {
@@ -119,19 +119,20 @@ const promptEmployee = () => {
         }
         return true;
       },
+      when: (answer) => answer.role === "Engineer",
     },
 
     {
-        type: "input",
-        name: "school",
-        message: "Please enter the employee's school",
-        validate: async (answer) => {
-          if (!answer) {
-            return "Please enter a valid school";
-          }
-          return true;
-        },
+      type: "input",
+      name: "school",
+      message: "Please enter the employee's school",
+      validate: async (answer) => {
+        if (!answer) {
+          return "Please enter a valid school";
+        }
+        return true;
       },
+      when: (answer) => answer.role === "Intern",
+    },
   ]);
 };
-
